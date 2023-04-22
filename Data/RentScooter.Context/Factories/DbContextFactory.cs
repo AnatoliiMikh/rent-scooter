f22fcb2e1cf83258c38a8788898c9c11b1338a49
AnatoliiMikh <361111@bk.ru>
2023-04-22 18:41:54 +0300

    Add all db f except DbSeeder, MainDbContext, Context.Entities + Shared.Settings

--- Data/RentScooter.Context/Factories/DbContextFactory.cs
+++ Data/RentScooter.Context/Factories/DbContextFactory.cs
@@ -1,5 +1,18 @@
 ﻿namespace RentScooter.Context;
 
+using Microsoft.EntityFrameworkCore;
+
 public class DbContextFactory
 {
+    private readonly DbContextOptions<MainDbContext> options;
+
+    public DbContextFactory(DbContextOptions<MainDbContext> options)
+    {
+        this.options = options;
+    }
+
+    public MainDbContext Create()
+    {
+        return new MainDbContext(options);
+    }
 }
--- Data/RentScooter.Context/MainDbContext.cs
+++ Data/RentScooter.Context/MainDbContext.cs
@@ -1,6 +1,12 @@
-﻿namespace RentScooter.Context
+﻿using RentScooter.Context.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace RentScooter.Context
 {
-    public class MainDbContext
+    public class MainDbContext : IdentityDbContext<User, UserRole, Guid>
     {
+
     }
 }
