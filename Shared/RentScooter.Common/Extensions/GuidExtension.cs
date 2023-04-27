c7c7b95827d95f78790e2217901816598faf55c0
AnatoliiMikh <361111@bk.ru>
2023-04-27 17:26:17 +0300

    Add Common to Shared

---
+++ Shared/RentScooter.Common/Extensions/GuidExtension.cs
@@ -0,0 +1,11 @@
+﻿namespace RentScooter.Common.Extensions;
+
+using System;
+
+public static class GuidExtension
+{
+    public static string Shrink(this Guid guid)
+    {
+        return guid.ToString().Replace("-", "").Replace(" ", "");
+    }
+}
