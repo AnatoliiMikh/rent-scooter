c5d45091b3cf524ffe857885b716ec4413a705b4
AnatoliiMikh <361111@bk.ru>
2023-04-29 13:19:04 +0300

    Brands controller created

---
+++ Systems/Api/RentScooter.Api/Controllers/Brands/Models/BrandResponse.cs
@@ -0,0 +1,18 @@
+﻿namespace RentScooter.API.Controllers.Brands.Models;
+
+using AutoMapper;
+using RentScooter.Services.Brands;
+
+public class BrandResponse
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+}
+
+public class BrandResponseProfile : Profile
+{
+    public BrandResponseProfile()
+    {
+        CreateMap<BrandModel, BrandResponse>();
+    }
+}
