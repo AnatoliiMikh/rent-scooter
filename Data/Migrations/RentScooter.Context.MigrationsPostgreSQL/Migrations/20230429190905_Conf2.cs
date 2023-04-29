257f8737255b2d3657e7c6ad6a33a8b7e3819796
AnatoliiMikh <361111@bk.ru>
2023-04-29 22:45:51 +0300

    Everything is working, except of Web-BD + No Revenue

---
+++ Data/Migrations/RentScooter.Context.MigrationsPostgreSQL/Migrations/20230429190905_Conf2.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RentScooter.Context.MigrationsPostgreSQL.Migrations
+{
+    /// <inheritdoc />
+    public partial class Conf2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
